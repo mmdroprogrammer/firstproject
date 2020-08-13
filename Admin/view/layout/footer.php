e3e24607dd6ed113e63c0cc0352f93392a8d7856
mohammad rohollahnezhad <mohammad.ro565@yahoo.com>
2020-08-13 18:59:33 +0430

    footer copyright

--- Admin/view/layout/footer.php
+++ Admin/view/layout/footer.php
@@ -11,11 +11,8 @@
   <!-- Main Footer -->
   <footer class="main-footer">
     <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
     <!-- Default to the left -->
-    <strong>CopyLeft &copy; 2018 <a href="http://github.com/hesammousavi/">حسام موسوی</a>.</strong>
+    <strong>CopyLeft &copy; 2025 <a href="http://github.com/hesammousavi/">محمد روح الله نژاد</a>.</strong>
   </footer>
 </div>
 <!-- ./wrapper -->
