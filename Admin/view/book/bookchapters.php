e12fb81f942c3d899a26af2e9f14fb5bd9932c7a
Mohammad Rohollahnezhad <mohammad.ro565@yahoo.com>
2020-08-25 15:44:13 +0430

    create book chapters for super admin

---
+++ Admin/view/book/bookchapters.php
@@ -0,0 +1,33 @@
+<form action="">
+    <legend>تعریف فصول کتاب</legend>
+    <div class="container" style="margin-right: 15%;">
+
+        <div class="row" dir="rtl">
+
+            <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5" dir="rtl">
+                <div class="form-group">
+                    <label for="">نام کتاب :</label>
+                    <select name="" id="input" class="form-control">
+                        <option value="">هندسه</option>
+                        <option value="">حسابان </option>
+                        <option value="">جبر و احتمالات</option>
+                    </select>
+                </div>
+            </div>
+
+        </div>
+        <div class="row" dir="rtl">
+
+            <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5" dir="rtl">
+                <div class="form-group">
+                    <label for="">نام فصل :</label>
+                    <input type="text" class="form-control" id="" placeholder="لطفا نام دانش آموز را وارد کنید">
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="col-md-12 text-center">
+        <button id="singlebutton" name="singlebutton" class="btn btn-primary" style="margin-bottom: 10px;">ثبت</button>
+    </div>
+</form>
