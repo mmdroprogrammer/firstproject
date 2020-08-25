cec1b41d683bbc84125974741caf003a5b3e4181
Mohammad Rohollahnezhad <mohammad.ro565@yahoo.com>
2020-08-25 15:43:47 +0430

    create add book for super admin

---
+++ Admin/view/book/addbook.php
@@ -0,0 +1,46 @@
+<form action="">
+    <legend>افزودن کتاب</legend>
+    <div class="container" style="margin-right: 15%;">
+
+        <div class="row" dir="rtl">
+
+            <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5" dir="rtl">
+                <div class="form-group">
+                    <label for="">گروه کتاب</label>
+                    <select name="" id="input" class="form-control">
+                        <option value="">اصلی</option>
+                        <option value="">کمک آموزشی </option>
+                        <option value="">تست</option>
+                    </select>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="container" style="margin-right: 15%;">
+
+        <div class="row" dir="rtl">
+
+            <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5" dir="rtl">
+                <div class="form-group">
+                    <label for="">نام کتاب :</label>
+                    <input type="text" class="form-control" id="" placeholder="لطفا نام دانش آموز را وارد کنید">
+                </div>
+            </div>
+
+        </div>
+        <div class="row" dir="rtl">
+
+            <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5" dir="rtl">
+                <div class="form-group">
+                    <label for="">سال انتشار :</label>
+                    <input type="text" class="form-control" id="" placeholder="لطفا نام دانش آموز را وارد کنید">
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="col-md-12 text-center">
+        <button id="singlebutton" name="singlebutton" class="btn btn-primary" style="margin-bottom: 10px;">ثبت</button>
+    </div>
+</form>
